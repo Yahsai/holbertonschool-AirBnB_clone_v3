a88d2c8dc51d30ed99a4734f0a7083cf1b3c0452
Lyan <lyanosorio98@gmail.com>
2024-04-20 02:43:52 -0400

    pycodestyle of amenities, city, place, review

--- tests/test_console.py
+++ tests/test_console.py
@@ -33,7 +33,8 @@ class TestConsoleDocs(unittest.TestCase):
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None, "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1, "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
@@ -41,5 +42,6 @@ class TestConsoleDocs(unittest.TestCase):
             HBNBCommand.__doc__, None, "HBNBCommand class needs a docstring"
         )
         self.assertTrue(
-            len(HBNBCommand.__doc__) >= 1, "HBNBCommand class needs a docstring"
+            len(HBNBCommand.__doc__) >= 1,
+            "HBNBCommand class needs a docstring"
         )
