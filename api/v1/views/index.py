ab60111e9305aaa080e5588798a7a6e50011bce8
Yahsai <yahsaisantana1@gmail.com>
2024-04-21 22:35:48 -0400

    Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Index of views"""
 
+
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -12,15 +13,15 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status", methods=["GET"], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Status of API"""
+    """ Status of API """
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
-    """Retrieves the number of each objects by type"""
+    """ Retrieves the number of each objects by type """
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
