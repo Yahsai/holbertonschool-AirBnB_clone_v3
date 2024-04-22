13036161665dabcefc4210acf7cba5731d825eb5
Yahsai <yahsaisantana1@gmail.com>
2024-04-21 22:45:19 -0400

    Update

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,17 +8,13 @@ import inspect
 import models
 from models import user
 from models.base_model import BaseModel
-import os
-import pep8
 import pycodestyle
 import unittest
-
 User = user.User
 
 
 class TestUserDocs(unittest.TestCase):
     """Tests to check the documentation and style of User class"""
-
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
@@ -27,45 +23,42 @@ class TestUserDocs(unittest.TestCase):
     def test_pep8_conformance_user(self):
         """Test that models/user.py conforms to PEP8."""
         pep8s = pycodestyle.StyleGuide(quiet=True)
-        result = pep8s.check_files(["models/user.py"])
-        self.assertEqual(
-            result.total_errors, 0, "Found code style errors (and warnings)."
-        )
+        result = pep8s.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_user(self):
         """Test that tests/test_models/test_user.py conforms to PEP8."""
         pep8s = pycodestyle.StyleGuide(quiet=True)
-        result = pep8s.check_files(["tests/test_models/test_user.py"])
-        self.assertEqual(
-            result.total_errors, 0, "Found code style errors (and warnings)."
-        )
+        result = pep8s.check_files(['tests/test_models/test_user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_user_module_docstring(self):
         """Test for the user.py module docstring"""
-        self.assertIsNot(user.__doc__, None, "user.py needs a docstring")
-        self.assertTrue(len(user.__doc__) >= 1, "user.py needs a docstring")
+        self.assertIsNot(user.__doc__, None,
+                         "user.py needs a docstring")
+        self.assertTrue(len(user.__doc__) >= 1,
+                        "user.py needs a docstring")
 
     def test_user_class_docstring(self):
         """Test for the City class docstring"""
-        self.assertIsNot(User.__doc__, None, "User class needs a docstring")
-        self.assertTrue(len(User.__doc__) >= 1, "User class needs a docstring")
+        self.assertIsNot(User.__doc__, None,
+                         "User class needs a docstring")
+        self.assertTrue(len(User.__doc__) >= 1,
+                        "User class needs a docstring")
 
     def test_user_func_docstrings(self):
         """Test for the presence of docstrings in User methods"""
         for func in self.user_f:
-            self.assertIsNot(
-                func[1].__doc__, None,
-                "{:s} method needs a docstring".format(func[0])
-            )
-            self.assertTrue(
-                len(func[1].__doc__) >= 1,
-                "{:s} method needs a docstring".format(func[0]),
-            )
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
 
 
 class TestUser(unittest.TestCase):
     """Test the User class"""
-
     def test_is_subclass(self):
         """Test that User is a subclass of BaseModel"""
         user = User()
@@ -74,46 +67,38 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(user, "created_at"))
         self.assertTrue(hasattr(user, "updated_at"))
 
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
-                     "Testing DBStorage")
     def test_email_attr(self):
         """Test that User has attr email, and it's an empty string"""
         user = User()
         self.assertTrue(hasattr(user, "email"))
-        if models.storage_t == "db":
+        if models.storage_t == 'db':
             self.assertEqual(user.email, None)
         else:
             self.assertEqual(user.email, "")
 
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
-                     "Testing DBStorage")
     def test_password_attr(self):
         """Test that User has attr password, and it's an empty string"""
         user = User()
         self.assertTrue(hasattr(user, "password"))
-        if models.storage_t == "db":
+        if models.storage_t == 'db':
             self.assertEqual(user.password, None)
         else:
             self.assertEqual(user.password, "")
 
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
-                     "Testing DBStorage")
     def test_first_name_attr(self):
         """Test that User has attr first_name, and it's an empty string"""
         user = User()
         self.assertTrue(hasattr(user, "first_name"))
-        if models.storage_t == "db":
+        if models.storage_t == 'db':
             self.assertEqual(user.first_name, None)
         else:
             self.assertEqual(user.first_name, "")
 
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
-                     "Testing DBStorage")
     def test_last_name_attr(self):
         """Test that User has attr last_name, and it's an empty string"""
         user = User()
         self.assertTrue(hasattr(user, "last_name"))
-        if models.storage_t == "db":
+        if models.storage_t == 'db':
             self.assertEqual(user.last_name, None)
         else:
             self.assertEqual(user.last_name, "")
